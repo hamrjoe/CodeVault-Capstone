ca70265f1bdee04229c34182597a1ecf99a1b410
Becky Plas <becky.plas@gmail.com>
2021-12-07 14:29:50 -0500

    updated DB

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -93,18 +93,24 @@ CREATE TABLE examples_tags (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO tags (tag_name) VALUES ('If Statement');
-INSERT INTO tags (tag_name) VALUES ('Loop');
-INSERT INTO tags (tag_name) VALUES ('Console');
+INSERT INTO tags (tag_name) VALUES ('If Statement'); --tag 1
+INSERT INTO tags (tag_name) VALUES ('Loop'); -- tag 2
+INSERT INTO tags (tag_name) VALUES ('Console'); -- tag 3
 
-INSERT INTO languages (language_name) VALUES ('Java');
-INSERT INTO languages (language_name) VALUES ('JavaScript');
-INSERT INTO languages (language_name) VALUES ('Python');
+INSERT INTO languages (language_name) VALUES ('Java'); -- laguage 1
+INSERT INTO languages (language_name) VALUES ('JavaScript'); -- laguage 2
+INSERT INTO languages (language_name) VALUES ('Python');  --language 3
 
 INSERT INTO examples (title, language_id, code_example, is_private, attribution, is_default) VALUES ('for loop', 1, '102,111,114,32,40,105,59,32,105,60,108,101,110,103,116,104,59,32,105,43,43,41,32,123,10,32,32,32,32,47,47,100,111,115,116,117,102,102,59,10,125', true, 'bob', true);
 INSERT INTO examples (title, language_id, code_example, is_private, attribution, is_default) VALUES ('if else statement', 1, '105,102,32,40,116,114,117,101,41,32,123,10,32,32,47,47,100,111,32,115,116,117,102,102,59,10,125,10,101,108,115,101,32,123,10,32,32,47,47,100,111,32,111,116,104,101,114,32,115,116,117,102,102,59,10,125', true, 'bob', true);
 INSERT INTO examples (title, language_id, code_example, is_private, attribution, is_default) VALUES ('if else statement', 2, '105,102,32,40,116,114,117,101,41,32,123,10,32,32,47,47,100,111,32,115,116,117,102,102,59,10,125,10,101,108,115,101,32,123,10,32,32,47,47,100,111,32,111,116,104,101,114,32,115,116,117,102,102,59,10,125', true, 'bob', true);
 INSERT INTO examples (title, language_id, code_example, is_private, attribution, is_default) VALUES ('console log', 2, '99,111,110,115,111,108,101,46,108,111,103,40,39,112,114,105,110,116,32,116,104,105,110,103,115,32,104,101,114,101,39,41', true, 'bob', true);
 INSERT INTO examples (title, language_id, code_example, is_private, attribution, is_default) VALUES ('System Out Print Line', 1, '83,121,115,116,101,109,46,111,117,116,46,112,114,105,110,116,108,110,40,39,115,97,121,32,116,104,105,110,103,115,32,104,101,114,101,39,41', true, 'bob', true);
+
+INSERT INTO examples_tags (example_id, tag_id) VALUES (1, 2);
+INSERT INTO examples_tags (example_id, tag_id) VALUES (2, 1);
+INSERT INTO examples_tags (example_id, tag_id) VALUES (3, 1);
+INSERT INTO examples_tags (example_id, tag_id) VALUES (4, 3);
+INSERT INTO examples_tags (example_id, tag_id) VALUES (5, 3);
 
 COMMIT TRANSACTION;
